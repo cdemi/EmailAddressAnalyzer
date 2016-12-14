866fd97eb71ded50fdbad379f6844da203babcba
CORP\christopher.demicoli <christopher.demicoli@gaminginnovationgroup.com>
2016-12-14 21:10:02 +0100

    Proof of Concept

---
+++ src/EmailAddressAnalyzer/Analyzer.cs
@@ -0,0 +1,101 @@
+﻿using DnsClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailAddressAnalyzer
+{
+    public enum FailReason
+    {
+        NoMailServerExists,
+        InvalidEmail,
+        NonExistentDomain
+    }
+    public enum WellKnownMailServer
+    {
+        Unknown,
+        Outlook,
+        Gmail,
+        Office365
+    }
+    public class EmailAddressAnalysis
+    {
+        public bool LooksGood;
+        public WellKnownMailServer WellKnownMailServer;
+        public FailReason Reason;
+    }
+    public static class Analyzer
+    {
+        const string emailAddressRegex = "(.+?)(@)(.+)";
+
+        private static LookupClient lookupClient = new LookupClient();
+
+        public async static Task<EmailAddressAnalysis> Analyze(string emailAddress)
+        {
+            var regexMatch = Regex.Match(emailAddress, emailAddressRegex);
+
+            if (regexMatch.Groups.Count == 4)
+            {
+                var domain = regexMatch.Groups[3].Value;
+
+                var dnsResponse = await lookupClient.QueryAsync(domain, QueryType.MX);
+
+                if (dnsResponse.Header.ResponseCode == DnsResponseCode.NotExistentDomain)
+                {
+                    return new EmailAddressAnalysis()
+                    {
+                        LooksGood = false,
+                        WellKnownMailServer = WellKnownMailServer.Unknown,
+                        Reason = FailReason.NonExistentDomain
+                    };
+                }
+
+                var mailHosts = dnsResponse.Answers.Select(a => a.RecordToString());
+
+                if (mailHosts.Count() > 0)
+                {
+                    var wellKnownMailServer = deduceMailServer(mailHosts);
+
+                    return new EmailAddressAnalysis()
+                    {
+                        LooksGood = true,
+                        WellKnownMailServer = wellKnownMailServer
+                    };
+                }
+                else
+                {
+                    return new EmailAddressAnalysis()
+                    {
+                        LooksGood = false,
+                        WellKnownMailServer = WellKnownMailServer.Unknown,
+                        Reason = FailReason.NoMailServerExists
+                    };
+                }
+            }
+            else
+            {
+                return new EmailAddressAnalysis()
+                {
+                    LooksGood = false,
+                    WellKnownMailServer = WellKnownMailServer.Unknown,
+                    Reason = FailReason.InvalidEmail
+                };
+            }
+        }
+
+        private static WellKnownMailServer deduceMailServer(IEnumerable<string> mailHosts)
+        {
+
+            if (mailHosts.Where(mh => mh.Contains("hotmail.com")).Any())
+                return WellKnownMailServer.Outlook;
+            else if (mailHosts.Where(mh => mh.Contains("outlook.com")).Any())
+                return WellKnownMailServer.Office365;
+            else if (mailHosts.Where(mh => mh.Contains("google.com")).Any())
+                return WellKnownMailServer.Gmail;
+            else
+                return WellKnownMailServer.Unknown;
+        }
+    }
+}
---
+++ test/EmailAddressAnalyzer.Tests/Tests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task TestOutlook()
+        {
+            string emailAddress = "christopher.demicoli@outlook.com";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Outlook);
+        }
+
+        [Fact]
+        public async Task TestGmail()
+        {
+            string emailAddress = "christopher.demicoli@gmail.com";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Gmail);
+        }
+
+        [Fact]
+        public async Task TestGoogleApps()
+        {
+            string emailAddress = "test@google.com";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Gmail);
+        }
+
+        [Fact]
+        public async Task TestOffice365()
+        {
+            string emailAddress = "test@microsoft.com";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Office365);
+        }
+
+        [Fact]
+        public async Task TestTestDomain()
+        {
+            string emailAddress = "test@test.com";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Unknown);
+        }
+
+        [Fact]
+        public async Task TestDomainWithoutMailServer()
+        {
+            string emailAddress = "test@99bits.net";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(!analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Unknown && analysis.Reason == EmailAddressAnalyzer.FailReason.NoMailServerExists);
+        }
+
+        [Fact]
+        public async Task TestInexistentDomain()
+        {
+            string emailAddress = "test@sdfgsdfgdf3355.net";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(!analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Unknown && analysis.Reason == EmailAddressAnalyzer.FailReason.NonExistentDomain);
+        }
+
+        [Fact]
+        public async Task TestNoDomain()
+        {
+            string emailAddress = "christopher.demicoli";
+            var analysis = await EmailAddressAnalyzer.Analyzer.Analyze(emailAddress);
+            Assert.True(!analysis.LooksGood && analysis.WellKnownMailServer == EmailAddressAnalyzer.WellKnownMailServer.Unknown && analysis.Reason == EmailAddressAnalyzer.FailReason.InvalidEmail);
+        }
+    }
+}
